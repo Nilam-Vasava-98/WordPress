b84bc3ebcf89af48b3d364b5b0b0d8fe73f8607e
Nilam <dev.nilamvasava1998@gmail.com>
2024-06-26 15:09:40 +0530

    Update footer.php

--- WordPress_Task/wp-content/themes/my-custom-theme/footer.php
+++ WordPress_Task/wp-content/themes/my-custom-theme/footer.php
@@ -1,18 +1,6 @@
 </div> <!-- End of #content -->
     <footer class="site-footer">
         <div class="container">
-            <div class="footer-widgets">
-                <?php if (is_active_sidebar('footer-1')) : ?>
-                    <div class="footer-widget-area">
-                        <?php dynamic_sidebar('footer-1'); ?>
-                    </div>
-                <?php endif; ?>
-                <?php if (is_active_sidebar('footer-2')) : ?>
-                    <div class="footer-widget-area">
-                        <?php dynamic_sidebar('footer-2'); ?>
-                    </div>
-                <?php endif; ?>
-            </div>
             <div class="footer-bottom">
                 <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. All rights reserved.</p>
             </div>
