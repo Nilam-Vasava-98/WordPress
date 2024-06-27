91961f5d67b3b24973c4572db81822cbd4b4048a
Nilam <dev.nilamvasava1998@gmail.com>
2024-06-27 09:56:21 +0530

    update header structure and style

--- WordPress_Task/wp-content/themes/my-custom-theme/header.php
+++ WordPress_Task/wp-content/themes/my-custom-theme/header.php
@@ -7,28 +7,21 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-    <header>
-        <div class="header-container">
-            <div class="header-logo-nav">
-                <!-- Logo -->
-                <div class="logo">
-                    <?php $uploads = wp_upload_dir(); ?>
-                    <a href="<?php echo home_url(); ?>">
-                        <img src="<?php echo esc_url($uploads['url'] . '/images.png'); ?>" alt="<?php bloginfo('name'); ?>">
-                    </a>
-                </div>
-                <!-- Mobile Menu Toggle -->
-                <div class="mobile-menu-toggle">
-                    <span>&#9776;</span>
-                </div>
-                <!-- Navigation Menu -->
-                <nav class="main-navigation">
-                    <?php wp_nav_menu(array('theme_location' => 'main-menu')); ?>
-                </nav>
-            </div>
-            <div class="header-description">
-                <p><?php bloginfo('description'); ?></p>
-            </div>
+<header>
+    <div class="header-container">
+        <div class="logo">
+            <?php $uploads = wp_upload_dir(); ?>
+            <a href="<?php echo home_url(); ?>">
+                <img src="<?php echo esc_url($uploads['url'] . '/images.png'); ?>" alt="<?php bloginfo('name'); ?>">
+            </a>
         </div>
-    </header>
+        <div class="mobile-menu-toggle">
+            <span>&#9776;</span>
+        </div>
+        <nav class="main-navigation">
+            <?php wp_nav_menu(array('theme_location' => 'main-menu')); ?>
+        </nav>
+    </div>
+</header>
+
     <div id="content">
